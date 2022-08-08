87739e56a6580b89d51659815303e89a26c66b8d
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-08-08 06:29:52 -0700

    chapter 9 done

---
+++ 09/server.js
@@ -0,0 +1,21 @@
+// model view controller (MVC) design pattern
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+const { logger } = require("./middleware/logEvents");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+const corsOption = require("./config/corsOptions")
+app.use(logger);
+app.use(cors(corsOption));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
+app.all("*", (req, res) => {
+  res.send("Not found(404)");
+});
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
